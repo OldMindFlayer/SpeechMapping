444a0c78e41895162ed975f82317d43b9319f74f
OldMindFlayer <OldMindFlayer@gmail.com>
2020-03-11 05:36:46 +0300

    add test3

--- tests/test3.py
+++ tests/test3.py
@@ -6,15 +6,46 @@ Created on Sat Mar  7 14:32:30 2020
 """
 import h5py
 import numpy as np
+from scipy.stats import describe
+import matplotlib.pyplot as plt
 
 path = 'C:/Workspace/SpeechMapping/SpeechMapping/data/Patient1/10_03_20/experiment_data/experiment_data.h5'
 with h5py.File(path,'r+') as f1:
     keys = f1.keys()
     print(keys)
     for i in range(3):
-        dataset = f1[list(keys)[i]]['raw_data']
+        dataset = f1[list(keys)[i]]['raw_data'][()]
         print(list(keys)[i], dataset.shape)
-        for i in range(dataset.shape[0]//2048):
-            delta = dataset[(i+1)*2048, 69] - dataset[(i)*2048, 69]
-            error = abs(1 - delta)*2048
-            print(error)
+        timestaps = dataset[1000:9000, 69]
+        #sorted_indecies = np.argsort(dataset[:,69])
+        #dataset_new = dataset[sorted_indecies]
+        #timesteps_new = dataset_new[1000:9000, 69]
+        #forward_diff_new = np.diff(timesteps_new)
+        forward_diff = np.diff(timestaps)
+        #print(forward_diff_new)
+        #print(describe(forward_diff_new))
+        #ab = np.arange(0,8000)
+        #print(np.sum(np.abs(forward_diff_new - 0.000488) > 0.00001))
+        print(np.sum(np.abs(forward_diff - 0.000488) > 0.0001))
+        a = np.arange(0,7999)
+        plt.figure()
+        plt.plot(a, forward_diff)
+        plt.show()
+
+#path2 = 'C:/Workspace/SpeechMapping/Sysoeva/10_10_19/data_rest/experiment_data.h5'
+#with h5py.File(path2,'r+') as f1:
+#    dataset = f1['protocol1']['raw_data']
+#    print(dataset.shape)
+#    print(dataset[:,68])
+
+
+
+
+
+
+
+
+
+
+
+
